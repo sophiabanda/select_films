5cafe5b6ef46b2dd5dfffb9af2dce5308037dedd
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2023-08-02 14:23:38 -0400

    integrated API

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,48 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { FilmCard } from "../film-card/film-card";
 import { FilmDetails } from "../film-details/film-details";
 
 export const MainView = () => {
-  const [films, setFilms] = useState([
-    {
-      id: 1,
-      title: "Eloquent JavaScript",
-      image:
-        "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
-      author: "Marijn Haverbeke",
-    },
-    {
-      id: 2,
-      title: "Mastering JavaScript Functional Programming",
-      image:
-        "https://images-na.ssl-images-amazon.com/images/I/51WAikRq37L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-      author: "Federico Kereki",
-    },
-    {
-      id: 3,
-      title: "JavaScript: The Good Parts",
-      image:
-        "https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX381_BO1,204,203,200_.jpg",
-      author: "Douglas Crockford",
-    },
-    {
-      id: 4,
-      title: "JavaScript: The Definitive Guide",
-      image:
-        "https://images-na.ssl-images-amazon.com/images/I/51HbNW6RzhL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-      author: "David Flanagan",
-    },
-    {
-      id: 5,
-      title: "The Road to React",
-      image:
-        "https://images-na.ssl-images-amazon.com/images/I/41MBLi5a4jL._SX384_BO1,204,203,200_.jpg",
-      author: "Robin Wieruch",
-    },
-  ]);
+  const [films, setFilms] = useState([]);
   const [selectedFilm, setSelectedFilm] = useState(null);
   //state variable
+
+  useEffect(() => {
+    fetch(`https://sophia-films.herokuapp.com/films`)
+      .then((response) => response.json())
+      .then((data) => {
+        const filmsFromAPI = data.map((item) => {
+          return {
+            id: item._id,
+            director: item.Director,
+            title: item.Title,
+            genre: item.Genre,
+            image: item.filmPosterImage,
+            summary: item.Summary,
+            favorite: item.Favorite,
+          };
+        });
+        setFilms(filmsFromAPI);
+      });
+  }, []);
 
   if (selectedFilm) {
     return (
