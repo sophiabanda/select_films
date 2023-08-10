00f8a703cd47ce09151ef9197cd64340e0f56b42
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2023-08-10 09:14:01 -0400

    added back signup

---
+++ src/components/sign-up-view/sign-up-view.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+
+export const SignUpView = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthday, setBirthday] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const userData = {
+      Name: username,
+      Password: password,
+      Email: email,
+      Birthday: birthday,
+    };
+
+    fetch("https://sophia-films.herokuapp.com/users", {
+      method: "POST",
+      body: JSON.stringify(userData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((response) => {
+      if (response.ok) {
+        alert("Signup successful!");
+      } else {
+        alert("Signup failed.");
+      }
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          minLength="5"
+          required
+        ></input>
+      </label>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        ></input>
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        ></input>
+      </label>
+      <label>
+        Birthday:
+        <input
+          type="date"
+          value={birthday}
+          onChange={(e) => setBirthday(e.target.value)}
+        ></input>
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
